6518e5d6ada099c06abff43e9c78fe98b515e50c
Flotchet <flo.maisse@live.be>
2023-01-03 21:57:34 +0100

    Rewrote to accomodate 4*cpus !w out ram

--- data_acquisition/scraper.py
+++ data_acquisition/scraper.py
@@ -1,5 +1,6 @@
 import itertools 
 import time
+import os
 
 from multiprocessing import Pool
 
@@ -241,31 +242,162 @@ def immoweb_url_scrapping() -> None:
     #save the list as a csv
     pd.DataFrame(urls).to_csv("urls.csv", index = False)
 
-def immoweb_page_scrapping() -> None:
-    """Scrape the pages of the houses from immoweb.
-    """
+
+def scrape_html_with_selenium_with_save(urls : list[str] or str, folder_path : str = "./raw_html", time_stamp : bool = True) -> int:
+    """Scrape the html of the page with selenium.
+    and saves each file to a folder as name+time_stamp.html
+
+    Parameters
+    ----------
+    urls : list[str] or str
+        The urls of the page.
+
+    folder_path : str = "./raw_html"
+        The path to the folder where the files will be stored
+
+    time_stamp : bool = True
+        make a time stamp to each file
+
+    Returns
+    -------
+    int
+        The number of files saved
+
+    Raises
+    ------
+    Exception
+        If the url is not a string or a list of strings.
+
+    Exception
+        If the folder_path is not a string.
+
+    Exception
+        If the time_stamp is not a boolean.
     
-    urls = pd.read_csv("urls.csv").values.tolist()
-    urls = list(urls)
+    Exception
+        If the folder_path does not exist.
+
+    Exception
+        If the folder_path is not a folder.
+    
+    Exception
+        If the folder_path is not writable.
+    """
+
+    #check if the url is a string or a list of strings
+    if not isinstance(urls, str) and not isinstance(urls, list):
+        raise Exception("The url is not a string or a list of strings.")
+
+    #check if the folder_path is a string
+    if not isinstance(folder_path, str):
+        raise Exception("The folder_path is not a string.")
+
+    #check if the time_stamp is a boolean
+    if not isinstance(time_stamp, bool):
+        raise Exception("The time_stamp is not a boolean.")
+
+    #check if the folder_path exists
+    if not os.path.exists(folder_path):
+        raise Exception("The folder_path does not exist.")
+
+    #check if the folder_path is a folder
+    if not os.path.isdir(folder_path):
+        raise Exception("The folder_path is not a folder.")
+
+    #check if the folder_path is writable
+    if not os.access(folder_path, os.W_OK):
+        raise Exception("The folder_path is not writable.")
+
+    #check if the url is a string
+    if isinstance(urls, str):
+        urls = [urls]
+
+    #open the driver
+    driver = webdriver.Firefox()
+    for url in urls:
+        driver.get(url)
+        if time_stamp:
+            with open(f"{folder_path}/{url.split('/')[-1]}_{time.time()}.html", "w") as f:
+                f.write(driver.page_source)
+        else:
+            with open(f"{folder_path}/{url.split('/')[-1]}.html", "w") as f:
+                f.write(driver.page_source)
+    driver.close()
+
+    return len(urls)
+   
+
+def immoweb_page_scrapping(csv_name : str = "urls.csv", folder_path : str = "./raw_html", time_stamp : bool = True) -> None:
+    """Scrape the pages of the houses from immoweb
+    in a memory optimized way and store the files.
+
+    Parameters
+    ----------
+    csv_name : str = "urls.csv"
+        The path to the files that contains url
+
+    folder_path : str = "./raw_html"
+        The path to the folder where the files will be stored
+
+    time_stamp : bool = True
+        make a time stamp to each file
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    Exception
+        If the csv_name is not a string
+    Exception
+        If the csv_name does not finish by .csv
+    """
+
+    #check if the csv_name is a string
+    if not isinstance(csv_name, str):
+        raise Exception("The csv_name must be a string")
+
+    #check if the filename finish by .csv
+    if csv_name[-4:] != ".csv":
+        raise Exception("The file name must finish by .csv")
+
+    #read the csv file
+    urls = pd.read_csv(csv_name).values.tolist()
+    urls = urls
+
     #Flatten the list of list into a list
     urls = [item for sublist in urls for item in sublist]
 
-    interval = 4096
-    segmented = list(map(list, zip(*([iter(urls)]*interval))))
-    if len(urls) % interval != 0:
-        segmented.append(urls[-(len(urls) % interval):])
+    #create a folder to store the html files
+    if not os.path.exists("./raw_html"):
+        os.mkdir("./raw_html")
 
-    for i, urls in enumerate(segmented):
-        time.sleep(5)
-        html = multi_process_scrape_html_content_with_selenium(urls, 8)
+    #This task is IO limited so we can use a lot of process at the same
+    #time to speed up the process even if they are more process than the
+    #number of cpus
 
-        #transform the list of list of str to list of str
-        html = list(itertools.chain.from_iterable(html))
+    #Get the number of logical cpus
+    n_cpus = os.cpu_count()
+    n_process = n_cpus*4
 
-        #create a html file for each url with html content as content in the rew_html folder
-        for url, h in zip(urls, html):
-            with open(f"./raw_html/{url.split('/')[-1]}.html", "w") as f:
-                f.write(h)
+    #Divide the work with numpy as a list of list of urls
+    urls = np.array_split(urls, n_process)
+    #transform nparray to list
+    urls = [url.tolist() for url in urls]
+
+    folder_path = [folder_path]*n_process
+    time_stamp = [time_stamp]*n_process
+
+    #Create the pool of workers
+    with Pool(n_process) as pool:
+        pool.starmap(scrape_html_with_selenium_with_save, zip(urls, folder_path, time_stamp))
+
+
+
+
+def main():
+    pass
 
 
 if __name__ == "__main__":
