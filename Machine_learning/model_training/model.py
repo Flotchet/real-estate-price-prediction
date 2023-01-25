dad40b792c1e4b8ed6cb14947b0bd00fc60edeb5
Flotchet <flo.maisse@live.be>
2023-01-25 09:35:51 +0100

    push for ai

--- Machine_learning/model_training/model.py
+++ Machine_learning/model_training/model.py
@@ -5,83 +5,89 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
-#import scaler between 0 and 1
-from sklearn.preprocessing import MinMaxScaler
 
-#load data
-data = pd.read_csv('Machine_learning/data_for_regression.csv')
-data = data[data["To sell"] == True]
+def split_set(df)
 
-#exclude the data from brussels region Bruxelles-Capitale
-data_bx = data[data['zipcode'] < 1300]
+def make_sets(csv : str = "Machine_learning/data_for_regression.csv") -> dict[str : pd.DataFrame]:
+    #load data
+    data : pd.DataFrame = pd.read_csv(csv)
+    data                = data[data["To sell"] == True]
 
-#only take the data where 1299 < zipcode < 1500 Province du Brabant wallon
-data_bw = data[data['zipcode'] > 1299]
-data_bw = data_bw[data_bw['zipcode'] < 1500]
+    #exclude the data from brussels region Bruxelles-Capitale
+    data_bx : pd.DataFrame = data[data['zipcode'] < 1300]
 
-#only take the data where 1499 < zipcode < 2000 Province du Brabant flamand 
-data_bf = data[data['zipcode'] > 1499]
-data_bf = data_bf[data_bf['zipcode'] < 2000]
+    #only take the data where 1299 < zipcode < 1500 Province du Brabant wallon
+    data_bw : pd.DataFrame = data[data['zipcode'] > 1299]
+    data_bw                = data_bw[data_bw['zipcode'] < 1500]
 
-#only take the data where 1999 < zipcode < 3000 Province d'Anvers
-data_an = data[data['zipcode'] > 1999]
-data_an = data_an[data_an['zipcode'] < 3000]
+    #only take the data where 1499 < zipcode < 2000 Province du Brabant flamand 
+    data_bf : pd.DataFrame = data[data['zipcode'] > 1499]
+    data_bf                = data_bf[data_bf['zipcode'] < 2000]
 
-#only take the data where 2999 < zipcode < 3500 Province du Brabant flamand 2
-data_bf2 = data[data['zipcode'] > 2999]
-data_bf2 = data_bf2[data_bf2['zipcode'] < 3500]
+    #only take the data where 1999 < zipcode < 3000 Province d'Anvers
+    data_an : pd.DataFrame = data[data['zipcode'] > 1999]
+    data_an                = data_an[data_an['zipcode'] < 3000]
 
-#only take the data where 3499 < zipcode < 4000 Province de Limbourg
-data_li = data[data['zipcode'] > 3499]
-data_li = data_li[data_li['zipcode'] < 4000]
+    #only take the data where 2999 < zipcode < 3500 Province du Brabant flamand 2
+    data_bf2 : pd.DataFrame = data[data['zipcode'] > 2999]
+    data_bf2                = data_bf2[data_bf2['zipcode'] < 3500]
 
-#only take the data where 3999 < zipcode < 5000 Province de Liège
-data_lg = data[data['zipcode'] > 3999] 
-data_lg = data_lg[data_lg['zipcode'] < 5000]
+        #only take the data where 3499 < zipcode < 4000 Province de Limbourg
+    data_li : pd.DataFrame = data[data['zipcode'] > 3499]
+    data_li                = data_li[data_li['zipcode'] < 4000]
 
-#only take the data where 4999 < zipcode < 6000 Province de Namur
-data_na = data[data['zipcode'] > 4999]
-data_na = data_na[data_na['zipcode'] < 6000]
+    #only take the data where 3999 < zipcode < 5000 Province de Liège
+    data_lg : pd.DataFrame = data[data['zipcode'] > 3999] 
+    data_lg                = data_lg[data_lg['zipcode'] < 5000]
 
-#only take the data where 5999 < zipcode < 6600 Province du Hainaut 1
-data_ha1 = data[data['zipcode'] > 5999]
-data_ha1 = data_ha1[data_ha1['zipcode'] < 6600]
+    #only take the data where 4999 < zipcode < 6000 Province de Namur
+    data_na : pd.DataFrame = data[data['zipcode'] > 4999]
+    data_na                = data_na[data_na['zipcode'] < 6000]
 
-#only take the data where 6599 < zipcode < 7000 Province de Luxembourg
-data_lu = data[data['zipcode'] > 6599]
-data_lu = data_lu[data_lu['zipcode'] < 7000]
+    #only take the data where 5999 < zipcode < 6600 Province du Hainaut 1
+    data_ha1 : pd.DataFrame = data[data['zipcode'] > 5999]
+    data_ha1                = data_ha1[data_ha1['zipcode'] < 6600]
 
-#only take the data where 6999 < zipcode < 8000 Province du Hainaut 2
-data_ha2 = data[data['zipcode'] > 6999]
-data_ha2 = data_ha2[data_ha2['zipcode'] < 8000]
+    #only take the data where 6599 < zipcode < 7000 Province de Luxembourg
+    data_lu : pd.DataFrame = data[data['zipcode'] > 6599]
+    data_lu                = data_lu[data_lu['zipcode'] < 7000]
 
-#only take the data where 7999 < zipcode < 9000 Province de Flandre-Occidentale
-data_foc = data[data['zipcode'] > 7999]
-data_foc = data_foc[data_foc['zipcode'] < 9000]
+    #only take the data where 6999 < zipcode < 8000 Province du Hainaut 2
+    data_ha2 : pd.DataFrame = data[data['zipcode'] > 6999]
+    data_ha2                = data_ha2[data_ha2['zipcode'] < 8000]
 
-#only take the data where 8999 < zipcode < 10000 Province de Flandre-Orientale
-data_for = data[data['zipcode'] > 8999]
-data_for = data_for[data_for['zipcode'] < 10000]
+    #only take the data where 7999 < zipcode < 9000 Province de Flandre-Occidentale
+    data_foc : pd.DataFrame = data[data['zipcode'] > 7999]
+    data_foc                = data_foc[data_foc['zipcode'] < 9000]
 
+    #only take the data where 8999 < zipcode < 10000 Province de Flandre-Orientale
+    data_for : pd.DataFrame = data[data['zipcode'] > 8999]
+    data_for                = data_for[data_for['zipcode'] < 10000]
+
+
+    #make a dict with the dataframes
+    data_dict : dict[str : pd.DataFrame] = {
+                                            'Bruxelles-Capitale': data_bx, 
+                                            'Province du Brabant wallon': data_bw, 
+                                            'Province du Brabant flamand': data_bf, 
+                                            'Province d\'Anvers': data_an, 
+                                            'Province du Brabant flamand 2': data_bf2, 
+                                            'Province de Limbourg': data_li, 
+                                            'Province de Liège': data_lg, 
+                                            'Province de Namur': data_na, 
+                                            'Province du Hainaut 1': data_ha1, 
+                                            'Province de Luxembourg': data_lu, 
+                                            'Province du Hainaut 2': data_ha2, 
+                                            'Province de Flandre-Occidentale': data_foc, 
+                                            'Province de Flandre-Orientale': data_for
+                                           }
+
+    return data_dict
+
+def adapt_data
 s=0
-#make a dict with the dataframes
-data_dict = {'Bruxelles-Capitale': data_bx, 
-             'Province du Brabant wallon': data_bw, 
-             'Province du Brabant flamand': data_bf, 
-             'Province d\'Anvers': data_an, 
-             'Province du Brabant flamand 2': data_bf2, 
-             'Province de Limbourg': data_li, 
-             'Province de Liège': data_lg, 
-             'Province de Namur': data_na, 
-             'Province du Hainaut 1': data_ha1, 
-             'Province de Luxembourg': data_lu, 
-             'Province du Hainaut 2': data_ha2, 
-             'Province de Flandre-Occidentale': data_foc, 
-             'Province de Flandre-Orientale': data_for}
-
 #go trougth items 
 for key, data in data_dict.items():
-
 
     #replace None and NaN by False in Fully equipped kitchen, Furnished, Swimming pool and Open fire
     data['Fully equipped kitchen'] = data['Fully equipped kitchen'].fillna(False)
@@ -121,11 +127,6 @@ for key, data in data_dict.items():
     y = data['Price']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=123)
-
-
-
-
-
 
     # Create a XGBoost regressor
     xg_reg = xgb.XGBRegressor(objective ='reg:squarederror', colsample_bytree = .75, learning_rate = 0.045,
