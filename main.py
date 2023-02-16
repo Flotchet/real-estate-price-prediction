d4444a61fcf8cd2b28a6a14597b5e14ab829bb83
Flotchet <flo.maisse@live.be>
2023-02-16 16:08:22 +0100

    Rename folder to correspond to the pipeline

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ layout = [
             create_menu('Data preparation part', ['Data preparation for visualization','Data preparation for regression']),
             create_menu('Data visualisation part', ['Data visualisation']),
             create_menu('Machine learning part', ['Model']),
-            create_menu('deploiment part', []),
+            create_menu('deployment part', []),
             [sg.Text('rest')],
             [sg.Button('Run all', key = 'run all')],
             [sg.Button('Exit' , key = 'Exit')],
@@ -24,30 +24,31 @@ layout = [
 
 
 keys = {
-        'Full scraper_key': 'python3 data_acquisition/data_aquisition.py',
-        'URL scraper_key': 'python3 data_acquisition/a_Immoweb_URL__scraper/Immoweb_URL__scraper.py',
-        'HTML scraper_key': 'python3 data_acquisition/b_Immoweb_HTML_scraper/Immoweb_HTML_scraper.py',
+        'Full scraper_key': 'python3 a_data_acquisition/data_aquisition.py',
+        'URL scraper_key': 'python3 a_data_acquisition/a_Immoweb_URL__scraper/Immoweb_URL__scraper.py',
+        'HTML scraper_key': 'python3 a_data_acquisition/b_Immoweb_HTML_scraper/Immoweb_HTML_scraper.py',
         'Data extractor + csv_key': """
-        python3 data_acquisition/c_Immoweb_data_extractor/Immoweb_data_extractor.py
-        python3 data_acquisition/d_Immoweb_raw_csv_maker/Immoweb_raw_csv_maker.py
+        python3 a_data_acquisition/c_Immoweb_data_extractor/Immoweb_data_extractor.py
+        python3 a_data_acquisition/d_Immoweb_raw_csv_maker/Immoweb_raw_csv_maker.py
         """,
 
-        'Data preparation for visualization_key': 'python3 data_preparation/data_preparation_for_visualization.py',
-        'Data preparation for regression_key': 'python3 data_preparation/data_preparation_for_regression.py',
+        'Data preparation for visualization_key': 'python3 b_data_preparation/data_preparation_for_visualization.py',
+        'Data preparation for regression_key': 'python3 b_data_preparation/data_preparation_for_regression.py',
 
-        'Data visualisation_key': 'python3 data_visualization/data_visualization.py',
+        'Data visualisation_key': 'python3 c_data_visualization/data_visualization.py',
 
-        'Model_key': 'Model',
+        'Model_key': 'd_Machine_learning/model_training/model.py',
 
-        'deploiment part_key': 'deployment/app.py',
+        'deployment part_key': 'e_deployment/app.py',
 
         'run all': 
         """
-        python3 data_acquisition/data_aquisition.py
-        python3 data_preparation/data_preparation_for_visualization.py
-        python3 data_preparation/data_preparation_for_regression.py
-        python3 data_visualization/data_visualization.py
-        python3 deployment/app.py
+        python3 a_data_acquisition/data_aquisition.py
+        python3 b_data_preparation/data_preparation_for_visualization.py
+        python3 b_data_preparation/data_preparation_for_regression.py
+        python3 c_data_visualization/data_visualization.py
+        python3 d_Machine_learning/model_training/model.py
+        python3 e_deployment/app.py
         """,
 
         'Exit': 'Exit',
