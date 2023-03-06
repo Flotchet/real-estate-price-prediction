3bc479380843e85dd076ca7c876c06ca7fe52a3f
Flotchet <flo.maisse@live.be>
2023-03-06 13:45:57 +0100

    Update the code

--- f_apache_airflow/automation.py
+++ f_apache_airflow/automation.py
@@ -1,10 +1,14 @@
 from airflow.models import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.hooks.bash_operator import BashOperator
 from datetime import datetime
 
-import os
+import sys
 #add the path to data_aquisition.py to the current working space
-os.chdir('a_data_acquisition')
+sys.path.append('a_data_acquisition')
+#add the path to data_preparation_for_regression.py to the current working space
+sys.path.append('b_data_preparation')
+#add the path to model.py to the current working space
+sys.path.append('d_Machine_learning/model_training')
 
 # Path: f_apache_airflow/automation.py
 
@@ -14,18 +18,21 @@ default_args = {
     'start_date': datetime(year = 2023, month = 3, day = 6)
     }
 
-dag = DAG('f_apache_airflow', default_args = default_args, schedule_interval = "0 20 * * *")
+dag = DAG('f_apache_airflow', default_args = default_args, schedule_interval = "0 12 32 * *")
 
 t1 = BashOperator(
-    task_id = 'print_date',
-    bash_command = 'date',
+    task_id = 'Scrape',
+    bash_command = 'python3 automation.py',
     dag = dag)
 
 t2 = BashOperator(
-    task_id = 'sleep',
-    bash_command = 'sleep 5',
-    retries = 3,
+    task_id = 'prepare',
+    bash_command = 'python3 data_preparation_for_regression.py',
     dag = dag)
 
-t1 >> t2
+t3 = BashOperator(
+    task_id = 'model',
+    bash_command = 'python3 model.py',
+    dag = dag)
 
+t1 >> t2 >> t3
